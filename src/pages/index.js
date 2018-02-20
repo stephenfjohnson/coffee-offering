c6b15ba10fa23d08c8ffee9362692badfabe37a1
Stephen Johnson <mail@stephenfjohnson.com>
2018-02-19 20:14:49 -0800

    Wellll more fixes to the contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,28 +75,14 @@ export default class IndexPage extends React.Component {
               <div class="field">
                 <label class="label">Name</label>
                 <div class="control">
-                  <input class="input" type="text" placeholder="Text input" />
+                  <input class="input" type="text" name="name" placeholder="Text input" />
                 </div>
-              </div>
-
-              <div class="field">
-                <label class="label">Username</label>
-                <div class="control has-icons-left has-icons-right">
-                  <input class="input is-success" type="text" placeholder="Text input" value="bulma" />
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-user" />
-                  </span>
-                  <span class="icon is-small is-right">
-                    <i class="fas fa-check" />
-                  </span>
-                </div>
-                <p class="help is-success">This username is available</p>
               </div>
 
               <div class="field">
                 <label class="label">Email</label>
                 <div class="control has-icons-left has-icons-right">
-                  <input class="input is-danger" type="email" placeholder="Email input" value="hello@" />
+                  <input class="input is-danger" type="email" name="email" placeholder="Email input" />
                   <span class="icon is-small is-left">
                     <i class="fas fa-envelope" />
                   </span>
@@ -110,16 +96,13 @@ export default class IndexPage extends React.Component {
               <div class="field">
                 <label class="label">Message</label>
                 <div class="control">
-                  <textarea class="textarea" placeholder="Textarea" />
+                  <textarea class="textarea" name="message" placeholder="Textarea" />
                 </div>
               </div>
 
               <div class="field is-grouped">
                 <div class="control">
                   <button class="button is-link">Submit</button>
-                </div>
-                <div class="control">
-                  <button class="button is-text">Cancel</button>
                 </div>
               </div>
             </form>
