b3ac85efc9efd3bf1ac09c728f7b1b0a9b585606
Stephen Johnson <mail@stephenfjohnson.com>
2018-02-19 21:10:30 -0800

    Ok take 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,30 @@ import Link from 'gatsby-link';
 import Script from 'react-load-script';
 import graphql from 'graphql';
 
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+}
+
 export default class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    fetch('/', { method: 'POST', headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, body: encode({ 'form-name': 'contact', ...this.state }) })
+      .then(() => alert('Success!'))
+      .catch(error => alert(error));
+    console.log(this.state);
+    e.preventDefault();
+  };
+
   handleScriptLoad() {
     if (typeof window !== `undefined` && window.netlifyIdentity) {
       window.netlifyIdentity.on('init', user => {
@@ -71,38 +94,40 @@ export default class IndexPage extends React.Component {
         </div>
         <div className="container">
           <div className="content">
-            <form name="contact" netlify>
-              <div class="field">
-                <label class="label">Name</label>
-                <div class="control">
-                  <input class="input" type="text" name="name" placeholder="Text input" />
+            <form name="contact" method="post" action="/thanks/" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.handleSubmit}>
+              <div className="field">
+                <label className="label">Name</label>
+                <div className="control">
+                  <input className="input" type="text" name="name" placeholder="Text input" onChange={this.handleChange} />
                 </div>
               </div>
 
-              <div class="field">
-                <label class="label">Email</label>
-                <div class="control has-icons-left has-icons-right">
-                  <input class="input is-danger" type="email" name="email" placeholder="Email input" />
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-envelope" />
+              <div className="field">
+                <label className="label">Email</label>
+                <div className="control has-icons-left has-icons-right">
+                  <input className="input is-danger" type="email" name="email" placeholder="Email input" onChange={this.handleChange} />
+                  <span className="icon is-small is-left">
+                    <i className="fas fa-envelope" />
                   </span>
-                  <span class="icon is-small is-right">
-                    <i class="fas fa-exclamation-triangle" />
+                  <span className="icon is-small is-right">
+                    <i className="fas fa-exclamation-triangle" />
                   </span>
                 </div>
-                <p class="help is-danger">This email is invalid</p>
+                <p className="help is-danger">This email is invalid</p>
               </div>
 
-              <div class="field">
-                <label class="label">Message</label>
-                <div class="control">
-                  <textarea class="textarea" name="message" placeholder="Textarea" />
+              <div className="field">
+                <label className="label">Message</label>
+                <div className="control">
+                  <textarea className="textarea" name="message" placeholder="Textarea" onChange={this.handleChange} />
                 </div>
               </div>
 
-              <div class="field is-grouped">
-                <div class="control">
-                  <button class="button is-link">Submit</button>
+              <div className="field is-grouped">
+                <div className="control">
+                  <button className="button is-link" type="submit">
+                    Submit
+                  </button>
                 </div>
               </div>
             </form>
