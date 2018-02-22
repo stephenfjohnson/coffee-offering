d81617f36561efb52af31fb3e83869477eaf9141
Stephen Johnson <mail@stephenfjohnson.com>
2018-02-21 22:14:49 -0800

    Fixed the Coffee Pages

--- src/templates/coffee-page.js
+++ src/templates/coffee-page.js
@@ -3,18 +3,19 @@ import graphql from 'graphql';
 import Helmet from 'react-helmet';
 import Content, { HTMLContent } from '../components/Content';
 
-export const CoffeePostTemplate = ({ content, contentComponent, description, title, helmet }) => {
+export const CoffeePostTemplate = ({ content, contentComponent, description, title, bags, origin, helmet }) => {
   const PostContent = contentComponent || Content;
 
   return (
     <section className="section">
       {helmet || ''}
       <div className="container content">
-        <h1>Coffee Page</h1>
         <div className="columns">
           <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">{title}</h1>
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">Offering: {title}</h1>
             <p>{description}</p>
+            <p>Bags: {bags}</p>
+            <p>Origin: {origin}</p>
             <PostContent content={content} />
           </div>
         </div>
@@ -33,6 +34,8 @@ export default ({ data }) => {
       description={post.frontmatter.description}
       helmet={<Helmet title={`Coffee | ${post.frontmatter.title}`} />}
       title={post.frontmatter.title}
+      bags={post.frontmatter.bags}
+      origin={post.frontmatter.origin}
     />
   );
 };
@@ -46,6 +49,8 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         title
         description
+        bags
+        origin
       }
     }
   }
