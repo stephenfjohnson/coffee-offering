91c75fef2dafd7451e5a553f31501d522df99eac
Stephen Johnson <mail@stephenfjohnson.com>
2018-04-09 20:44:28 -0700

    Fixed no tags

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import graphql from 'graphql';
+import PropTypes from 'prop-types';
+import { kebabCase } from 'lodash';
 import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
 import Content, { HTMLContent } from '../components/Content';
 
-export const BlogPostTemplate = ({
-  content, contentComponent, description, title, helmet,
-}) => {
+export const BlogPostTemplate = ({ content, contentComponent, description, title, helmet }) => {
   const PostContent = contentComponent || Content;
 
   return (
     <section className="section">
-      { helmet || ''}
+      {helmet || ''}
       <div className="container content">
         <div className="columns">
           <div className="column is-10 is-offset-1">
@@ -24,24 +24,43 @@ export const BlogPostTemplate = ({
   );
 };
 
-export default ({ data }) => {
-  const { markdownRemark: post } = data;
-
-  return (<BlogPostTemplate
-    content={post.html}
-    contentComponent={HTMLContent}
-    description={post.frontmatter.description}
-    helmet={<Helmet title={`Blog | ${post.frontmatter.title}`} />}
-    title={post.frontmatter.title}
-  />);
+BlogPostTemplate.propTypes = {
+  content: PropTypes.string.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.instanceOf(Helmet)
 };
 
+const BlogPost = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <BlogPostTemplate
+      content={post.html}
+      contentComponent={HTMLContent}
+      description={post.frontmatter.description}
+      helmet={<Helmet title={`${post.frontmatter.title} | Blog`} />}
+      tags={post.frontmatter.tags}
+      title={post.frontmatter.title}
+    />
+  );
+};
+
+BlogPost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object
+  })
+};
+
+export default BlogPost;
+
 export const pageQuery = graphql`
-  query BlogPostByPath($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
+  query BlogPostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
       html
       frontmatter {
-        path
         date(formatString: "MMMM DD, YYYY")
         title
         description
